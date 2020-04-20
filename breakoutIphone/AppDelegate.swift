ee744dd3e6aeb4b0bf9cd453a4b94d3e8fb0857f
Cheyo Jimenez <cheyo@masters3d.com>
2020-04-19 19:43:42 -0700

    turn off swift3 objc inference

--- breakoutIphone/AppDelegate.swift
+++ breakoutIphone/AppDelegate.swift
@@ -13,14 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                             
     var window: UIWindow?
 
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
 
            // Override point for customization after application launch.
                 return true
     }
-
-
 
     func applicationWillResignActive(_ application: UIApplication) {
     }
